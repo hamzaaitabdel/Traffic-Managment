9407505995f2f0b4dfb2755243945896a91ed06e
Hamza <hamza.90ha.haa.gmail.com>
2025-03-15 13:52:11 +0100

    first commit

---
+++ src/main/java/com/trafficoptimization/traffic_management/TrafficManagementApplication.java
@@ -0,0 +1,13 @@
+package com.trafficoptimization.traffic_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficManagementApplication.class, args);
+	}
+
+}
